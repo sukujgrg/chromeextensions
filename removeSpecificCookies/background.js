02c23bc22a9ea823d5cdf6be2284b5d14ae0bec3
Suku John George <sukujgrg@gmail.com>
2017-01-06 10:58:31 +1100

    removed few un-wanted lines

--- removeSpecificCookies/background.js
+++ removeSpecificCookies/background.js
@@ -1,12 +1,3 @@
-
-
-chrome.cookies.getAll({domain: "theaustralian.com.au"}, function(cookies) {
-    for(var i=0; i<cookies.length;i++) {
-      console.log(cookies[i]);
-      chrome.cookies.remove({url: "https://" + cookies[i].domain  + cookies[i].path, name: cookies[i].name});
-    }
-  });
-
 function removeSpecficCookies(domain_name) {
   chrome.cookies.getAll({domain: domain_name}, function(cookies) {
     for(var i=0; i<cookies.length;i++) {
