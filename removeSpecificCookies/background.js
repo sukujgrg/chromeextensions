944d9924aa19b70318b100f04f9db1c8cb286aa4
Suku John George <sukujgrg@gmail.com>
2017-01-06 08:06:06 +1100

    first commit
    a chrome extension for deleting specific cookies

---
+++ removeSpecificCookies/background.js
@@ -0,0 +1,24 @@
+
+
+chrome.cookies.getAll({domain: "theaustralian.com.au"}, function(cookies) {
+    for(var i=0; i<cookies.length;i++) {
+      console.log(cookies[i]);
+      chrome.cookies.remove({url: "https://" + cookies[i].domain  + cookies[i].path, name: cookies[i].name});
+    }
+  });
+
+function removeSpecficCookies(domain_name) {
+  chrome.cookies.getAll({domain: domain_name}, function(cookies) {
+    for(var i=0; i<cookies.length;i++) {
+      console.log(cookies[i]);
+      chrome.cookies.remove({url: "https://" + cookies[i].domain  + cookies[i].path, name: cookies[i].name});
+    }
+  });
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var domain_names = ["theaustralian.com.au", "news.com.au", "www.theaustralian.com.au", "myaccount.news.com.au"];
+  for (i = 0; i < domain_names.length; i++){
+    removeSpecficCookies(domain_names[i]);
+  }
+});
